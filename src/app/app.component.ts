7544acd8406b57f87837ba44a76f7b8d4ec23191
Daniel Cabanillas <danielpolux@gmail.com>
2026-09-04 16:09:30 -0500

    fix: ocultar layout principal en pantalla de login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { filter } from 'rxjs';
 import { HeaderComponent } from './layout/header/header.component';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
 import { ToastComponent } from './layout/toast/toast.component';
@@ -14,4 +15,13 @@ import { AuthService } from './core/services/auth.service';
 })
 export class AppComponent {
   auth = inject(AuthService);
+  private readonly router = inject(Router);
+
+  readonly esRutaAutenticacion = signal(this.router.url.startsWith('/auth/'));
+
+  constructor() {
+    this.router.events
+      .pipe(filter((evento): evento is NavigationEnd => evento instanceof NavigationEnd))
+      .subscribe(evento => this.esRutaAutenticacion.set(evento.urlAfterRedirects.startsWith('/auth/')));
+  }
 }
